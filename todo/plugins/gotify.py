28201bf2f2c9e7b19b2156d050f4ac2ac4b689c7
uglyboy <uglyboy@uglyboy.cn>
2024-03-14 10:12:45 +0800

    Add Gotify plugin for notifications

---
+++ todo/plugins/gotify.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+
+from gotify import Gotify as Notify
+
+from todo.core import TodoItem, TodoTxt
+from todo.project import BaseNotify
+from todo.utils import config
+
+
+@dataclass
+class Gotify(BaseNotify):
+    url: str = config.gotify_url
+    _client: Notify = field(init=False)
+
+    def __post_init__(self):
+        super().__post_init__()
+        self._client = Notify(base_url=self.url, app_token=self.id)
+
+    @staticmethod
+    def _validate(id: str):
+        pass
+
+    def __call__(self, todo: TodoItem, todotxt: TodoTxt, _=lambda x, _: x):
+        self._client.create_message(
+            todo.message,
+        )
--- todo/utils/config.py
+++ todo/utils/config.py
@@ -14,6 +14,8 @@ class Config:
     smtp_port: int = int(os.getenv("SMTP_PORT", 465))
     smtp_email: str = os.getenv("SMTP_EMAIL", "notice@uglyboy.cn")
     smtp_password: str = os.getenv("SMTP_PASSWORD", "kZGrkiv4YescJMTg")
+    # Gotify
+    gotify_url: str = os.getenv("GOTIFY_URL", "https://notify.uglyboy.cn")
 
 
 config = Config()
