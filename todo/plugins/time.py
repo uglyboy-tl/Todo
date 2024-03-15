85a92eef88bc9e38584ab1f80ccab008acc995bc
uglyboy <uglyboy@uglyboy.cn>
2024-03-15 15:18:54 +0800

    Add time plugin to pyproject.toml and implement Time context class

---
+++ todo/plugins/time.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+
+from loguru import logger
+
+from todo.core import TodoItem
+from todo.project import BaseContext, Option
+
+
+@dataclass
+class Time(BaseContext):
+    regex: str
+
+    def __call__(self, todo: TodoItem, process):
+        logger.debug(f"Checking: {todo}")
+        if not self._check(todo.context):
+            logger.debug(f"Skipping: {todo}")
+            process(todo, Option.BREAK)
+
+    def _check(self, contexts: List[str]):
+        for context in contexts:
+            if self.pattern.match(context):
+                time_period = context
+        # 解析时间段以获取开始时间和结束时间
+        start_time_str, end_time_str = time_period.split("-")
+        start_time = datetime.strptime(start_time_str, "%H:%M").time()
+        end_time = datetime.strptime(end_time_str, "%H:%M").time()
+        current_time = datetime.now().time()
+
+        if start_time > end_time:
+            return start_time <= current_time or current_time <= end_time
+        else:
+            return current_time >= start_time and current_time <= end_time
