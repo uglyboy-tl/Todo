7c3336fae5300e91c4693f08cfb2a0d9f8d33537
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-03-17 10:18:18 +0800

    Refactor DateFilter class to support multiple date filters

--- todo/plugins/system/filter/date.py
+++ todo/plugins/system/filter/date.py
@@ -14,27 +14,32 @@ HEADERS = {"User-Agent": "Mozilla/5.0 3578.98 Safari/537.36"}
 
 @dataclass
 class DateFilter(BaseFilter):
-    check: str = "holiday"
+    regex: str = r"^holiday|workday$"
 
     def __call__(self, todo: TodoItem, process):
-        if not self._check(todo.due):
+        if not self._check(todo.due, todo.context):
             todo.due += timedelta(days=1)
             process(todo, Option.BREAK)
 
-    def _check(self, time: datetime):
+    def _check(self, time: datetime, contexts: list[str]):
+        for context in contexts:
+            if self.pattern.match(context):
+                date_filter = context
+        if not date_filter:
+            return False
         try:
             url = request.Request(HOLIDAY_URL.format(time.strftime("%Y-%m-%d")), headers=HEADERS)
             with request.urlopen(url) as response:
                 content = response.read().decode("utf-8")
                 data = json.loads(content)
                 logger.trace(f"请求成功: {data}")
-            if self.check == "workday":
+            if date_filter == "workday":
                 return data["type"]["type"] in [0, 3]
-            elif self.check == "holiday":
+            elif date_filter == "holiday":
                 return data["type"]["type"] in [1, 2]
             else:
                 response = data["holiday"]
-                return response is not None and response["name"] == self.check
+                return response is not None and date_filter == response["name"]
         except error.URLError as e:
             logger.error(f"URL错误: {e.reason}")
         except Exception as e:
