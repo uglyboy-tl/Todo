97c32052cd1285c8d4ec5cf11a003b1b97002ad4
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-03-21 12:10:16 +0800

    Add active_scripts field to Alert class

--- todo/plugins/system/alert.py
+++ todo/plugins/system/alert.py
@@ -1,5 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime, timedelta
+from typing import List
 
 from todo.core import TodoItem, TodoTxt
 from todo.project import BaseContext, Option
@@ -8,6 +9,7 @@ from todo.project import BaseContext, Option
 @dataclass
 class Alert(BaseContext):
     alert_days: int = 1
+    active_scripts: List[str] = field(default_factory=list)
 
     def __call__(self, todo: TodoItem, process):
         if self.name in todo.context:
@@ -21,7 +23,9 @@ class Alert(BaseContext):
                 and todo.due.date() > datetime.now().date()
                 and todo.due.date() <= datetime.now().date() + timedelta(days=self.alert_days)
             ):
-                if todo.message and "#HIDDEN" not in todo.context:
+                if any(True for context in todo.context if context in self.active_scripts):
+                    process(todo, Option.EXECUTE)
+                elif todo.message and "#HIDDEN" not in todo.context:
                     diff = todo.due.date() - datetime.now().date()
                     notify = TodoItem(f"距离：`{todo.message.strip()}` 还有{diff.days}天 @notify")
                     process(notify, Option.EXECUTE)
